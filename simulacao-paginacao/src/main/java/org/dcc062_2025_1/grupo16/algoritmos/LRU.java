85e6f547512c966426b64a5789681056b43e148b
Kauê Paraízo <kaueparaizo@gmail.com>
2025-08-02 18:51:11 -0300

    Adaptação e atualização LRU

--- simulacao-paginacao/src/main/java/org/dcc062_2025_1/grupo16/algoritmos/LRU.java
+++ simulacao-paginacao/src/main/java/org/dcc062_2025_1/grupo16/algoritmos/LRU.java
@@ -1,5 +1,7 @@
 package org.dcc062_2025_1.grupo16.algoritmos;
 
+import java.util.LinkedHashMap;
+
 /**
  * Implementação da simulação do algoritmo LRU.
  */
@@ -7,11 +9,44 @@ public class LRU implements AlgoritmoSubstituicao {
 
     @Override
     public void simula(int[] paginas, int numeroFrames) {
-        throw new UnsupportedOperationException("Não implementado ainda.");
+
+        // Capacidade máxima do buffer
+        int capacidade = 3;
+
+        // Criação do buffer com capacidade descrita, fator de carga 0.75 e true como ordem de acesso
+        LinkedHashMap<Integer, Integer> buffer = new LinkedHashMap<>(capacidade, 0.75f, true);
+
+        // Contador de page faults 
+        int faults = 0;
+
+        // For para percorrer todas as páginas acessadas
+        for (int pagina : paginas) {
+
+            // Verifica se a página está no buffer 
+            if (!buffer.containsKey(pagina)) {
+
+                // Se o buffer está cheio, remove a página menos recentemente usada
+                if (buffer.size() == capacidade)
+                    buffer.remove(buffer.keySet().iterator().next());
+
+                // Incrementa o número de page faults
+                faults++;
+            }
+
+            // Adiciona ou atualiza a página no buffer 
+            buffer.put(pagina, 1);
+
+            // Printa o estado atual
+            System.out.println("Buffer: " + buffer.keySet());
+        }
+
+        // Printa o total de falhas
+        System.out.println("Page faults (LRU): " + faults);
     }
 
     @Override
     public String getNomeAlgoritmo() {
-        return "";
+        // Printa o tipo de algoritmo
+        return "LRU";
     }
 }
